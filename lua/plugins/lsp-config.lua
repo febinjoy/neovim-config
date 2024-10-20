ef51111708b9463f4f7d5ad6c40b53d224c41477
Febin Joy <febinjoy@gmail.com>
2024-10-20 19:08:38 +1100

    Add support for pyright and ts_ls

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -10,7 +10,9 @@ return {
     config = function()
       require('mason-lspconfig').setup({
         ensure_installed = {
-          "lua_ls"
+          "lua_ls",
+          "pyright",
+          "ts_ls",
         }
       })
     end
@@ -20,6 +22,8 @@ return {
     config = function()
       local lspconfig = require('lspconfig')
       lspconfig.lua_ls.setup({})
+      lspconfig.pyright.setup {}
+      lspconfig.ts_ls.setup {}
     end
   }
 }
