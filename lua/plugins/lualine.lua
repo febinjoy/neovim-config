b6214eae493e4cdf074dbe1c70b4166951fc3560
Febin Joy <febinjoy@gmail.com>
2024-10-20 15:52:51 +1100

    Add more comments to lualine plugin

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,4 +1,5 @@
 -- For adding the status bar
+-- Please refer https://github.com/nvim-lualine/lualine.nvim/blob/master/THEMES.md for themes
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
