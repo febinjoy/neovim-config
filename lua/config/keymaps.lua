c8688b86300897b25859a87e004fe9db3804d7c5
Febin Joy <febinjoy@gmail.com>
2024-10-21 22:13:23 +1100

    Add none-ls support

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -31,3 +31,5 @@ vim.keymap.set("n", "K", vim.lsp.buf.hover,{})
 vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
 vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
+
+vim.keymap.set('n', "<leader>gf", vim.lsp.buf.format, {})
