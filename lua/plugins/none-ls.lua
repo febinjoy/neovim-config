0336f65bb8e78f8a32bc127dbbd64f6489cf52b6
Febin Joy <febinjoy@gmail.com>
2024-10-22 21:13:44 +1100

    Add more linters and formatters

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,24 +1,32 @@
 return {
-  "nvimtools/none-ls.nvim",
-  dependencies = {
-    "nvimtools/none-ls-extras.nvim",
-  },
-  config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.diagnostics.pylint.with({
-          diagnostics_postprocess = function(diagnostic)
-            diagnostic.code = diagnostic.message_id
-          end,
-        }),
-        null_ls.builtins.formatting.black,
-        null_ls.builtins.formatting.isort,
-        null_ls.builtins.diagnostics.codespell,
-        require("none-ls.diagnostics.eslint_d"),
-      },
-    })
-  end,
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				-- Formatting
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.formatting.black,
+				null_ls.builtins.formatting.isort,
+        null_ls.builtins.formatting.htmlbeautifier,
+				-- Diagnostics
+				null_ls.builtins.diagnostics.write_good,
+				null_ls.builtins.diagnostics.pylint.with({
+					diagnostics_postprocess = function(diagnostic)
+						diagnostic.code = diagnostic.message_id
+					end,
+				}),
+				null_ls.builtins.diagnostics.revive,
+				null_ls.builtins.diagnostics.codespell,
+				null_ls.builtins.diagnostics.todo_comments,
+				null_ls.builtins.diagnostics.trail_space,
+				require("none-ls.diagnostics.eslint_d"),
+				-- Refactoring
+			},
+		})
+	end,
 }
