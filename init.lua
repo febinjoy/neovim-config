58afb3167b2256ad6fa48021f524b935cb2bae5b
Febin Joy <febinjoy@gmail.com>
2024-10-23 21:00:25 +1100

    Cleanup and refactoring init and plugins

--- init.lua
+++ init.lua
@@ -18,12 +18,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Options
 local opts = {}
-
--- Setup
-
--- lazy.nvim
 require("lazy").setup("plugins", opts)
 
 -- Set colorscheme
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,21 +1,21 @@
 -- Fuzzy Finder
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		branch = "0.1.x",
-		dependencies = { "nvim-lua/plenary.nvim" },
-	},
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		config = function()
-			require("telescope").setup({
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({}),
-					},
-				},
-			})
-			require("telescope").load_extension("ui-select")
-		end,
-	},
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("telescope").setup({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
+          },
+        },
+      })
+      require("telescope").load_extension("ui-select")
+    end,
+  },
 }
