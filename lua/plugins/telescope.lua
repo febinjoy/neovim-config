e93043a844614f7072e852cf67b349d1dcbe5496
Febin Joy <febinjoy@gmail.com>
2024-10-24 08:09:14 +1100

    Resolve issues with live grep

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,10 +6,23 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     branch = "0.1.x",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope-live-grep-args.nvim",
+      event = "VeryLazy",
+      config = function(_, _)
+        require("lazyvim.util").on_load("telescope.nvim", function()
+          require("telescope").load_extension("live_grep_args")
+        end)
+      end,
+    },
     config = function()
       require("telescope").setup({
         extensions = {
+          require("telescope-live-grep-args.actions"),
+          live_grep_args = {
+            auto_quoting = true,
+          },
           ["ui-select"] = {
             require("telescope.themes").get_dropdown({}),
           },
